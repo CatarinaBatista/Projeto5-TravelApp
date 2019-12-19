b57a05c4a99d7bb9504adab75c09794eec325258
Catarina Batista <catarinabatistayou@gmail.com>
2019-12-19 17:58:40 +0000

    Fill a modal with trip info

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,3 +1,43 @@
-import {getCityData} from './apis';
+import { getCityData, getImages} from './apis';
+import moment from 'moment';
 
-document.getElementById('submitForm').addEventListener('click', (event) => getCityData(event));
+/* Show a modal with data of the city*/
+const showCityData = async(event) => {
+    const form = document.getElementById('form');
+
+    if (form.checkValidity()) {
+        const city = await getCityData(event);
+    
+        fillModalInfo(city);
+        $('#modal').modal({show: true}); 
+    }
+    else {
+        alert('Please fill out the field "Going to", "Departing on" and "Returning on"');
+    }
+}
+
+/* Fill Modal with Trip Info*/
+const fillModalInfo = async(city) => {
+    const title = document.getElementById('modalTitle');
+    const startDate = document.getElementById('depart-date').value.toString();
+    const endDate = document.getElementById('return-date').value;
+    const from = document.getElementById('from').value;
+
+
+    if (from === '') {
+        title.innerText = `Going to ${city.cityName}, ${city.country}`;
+    }
+    else {
+        title.innerText = `${from}  -->  ${city.cityName}, ${city.country}`;
+    }
+
+    document.getElementById('departDate').innerText = moment(startDate).format("dddd, DD MMMM YYYY").toString();
+    document.getElementById('returnDate').innerText = moment(endDate).format("dddd, DD MMMM YYYY").toString();
+    document.getElementById('durationTrip').innerText = moment(endDate).diff(moment(startDate), 'days') + ' days';
+
+    document.getElementById('cityImg').setAttribute('src', await getImages(city.cityName));
+    document.getElementById('cityImg').setAttribute('alt', city.cityName);
+}
+
+/* Event Listener */
+document.getElementById('submitForm').addEventListener('click', (event) => showCityData(event));
