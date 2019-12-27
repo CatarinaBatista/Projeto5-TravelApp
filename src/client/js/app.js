a539892526855b87596a82f71876557f0e6fb5c1
Catarina Batista <catarinabatistayou@gmail.com>
2019-12-27 12:19:48 +0000

    Update app.js
    add global variabes,
    remove console.log of tests,
    add showAllTrips function to show all trips added to server...

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,7 +1,8 @@
 import { getCityData, getImages} from './apis';
 import moment from 'moment';
 
-let trip = {};
+let newTrip = {};
+const all = document.getElementById("allTrips");
 
 /* Show a modal with data of the city*/
 const showCityData = async(event) => {
@@ -51,7 +52,7 @@ const fillModalInfo = async(city) => {
     }
 
     // Trip
-    trip = {        
+    newTrip = {        
         city: city.cityName,
         country: city.country,
         latitude: city.latitude,
@@ -66,18 +67,17 @@ const fillModalInfo = async(city) => {
 const addTrip = async (event) => {
     event.preventDefault();
     
-    console.log(trip)
     const response = await fetch('http://localhost:8000/addTrip', {
         method: 'POST',
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ trip: trip })
+        body: JSON.stringify({ trip: newTrip })
     });
 
     try {
         const tripData = await response.json();
         
-        showTrip(tripData);
-        console.log(tripData)
+        showTrip(newTrip);
+
         return tripData;
 
     } catch (error) {
@@ -89,30 +89,28 @@ const addTrip = async (event) => {
 
 }
 
-const showTrip = async (tripData) => {
-    const all = document.getElementById("allTrips");
+const showTrip = async (trip) => {
     document.getElementById("section-trips").classList.remove("hidden");
-
-    console.log(document.getElementById("section-trips").classList)
     
-    console.log(trip.images[0])
-    all.innerHTML = 
+    const card = document.createElement("section");
+
+    card.innerHTML = 
     `<card class="card">
         <div class="carousel-inner" id="carousel">
             <div class="carousel-item active">
-                <img class="img-carousel" src="${trip.images[0].image}" alt="">
+                <img class="img-card" src="${trip.images[0].image}" alt="">
             </div>
             <div class="carousel-item">
-                <img class="img-carousel" src="${trip.images[1].image}" alt="">
+                <img class="img-card" src="${trip.images[1].image}" alt="">
             </div>
             <div class="carousel-item">
-                <img class="img-carousel" src="${trip.images[2].image}" alt="">
+                <img class="img-card" src="${trip.images[2].image}" alt="">
             </div>
             <div class="carousel-item">
-                <img class="img-carousel" src="${trip.images[3].image}" alt="">
+                <img class="img-card" src="${trip.images[3].image}" alt="">
             </div>
             <div class="carousel-item">
-                <img class="img-carousel" src="${trip.images[4].image}" alt="">
+                <img class="img-card" src="${trip.images[4].image}" alt="">
             </div>
         </div>
 
@@ -121,14 +119,67 @@ const showTrip = async (tripData) => {
             <p class="card-text">...</p>
             <button class="btn btn-danger">Delete</button>
         </div>
-    </card>`
+    </card>`;
+    
+    all.appendChild(card);
+}
+
+const showAllTrips = async () => {
+    const trips = await fetch('http://localhost:8000/getTrips')
+        .then(Response => Response.json());
+
+        try { 
+            if (trips.length > 0) {
+                for (const trip of trips) {                    
+                    const card = document.createElement("section");
+
+                    card.innerHTML = 
+                    `<card class="card">
+                        <div class="carousel-inner" id="carousel">
+                            <div class="carousel-item active">
+                                <img class="img-card" src="${trip.images[0].image}" alt="">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="img-card" src="${trip.images[1].image}" alt="">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="img-card" src="${trip.images[2].image}" alt="">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="img-card" src="${trip.images[3].image}" alt="">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="img-card" src="${trip.images[4].image}" alt="">
+                            </div>
+                        </div>
+
+                        <div class="card-body">
+                            <h5 class="card-title">${trip.city, trip.country}</h5>
+                            <p class="card-text">...</p>
+                            <button class="btn btn-danger">Delete</button>
+                        </div>
+                    </card>`;
+                    
+                    all.appendChild(card);
+                }    
+                            
+                document.getElementById("section-trips").classList.remove("hidden");                
+            }
+        }
+        catch (error) {
+            console.log('ERROR: ', error);
+            alert(error);
+        }
 }
 
 
 /* --- Event Listeners --- */
 
-/* Show modal with city info after submit form*/
+/* When app starts */
+showAllTrips();
+
+/* Show modal with city info after submit form */
 document.getElementById('submitForm').addEventListener('click', (event) => showCityData(event));
 
-/* Add trip after save it*/
+/* Add trip after save it */
 document.getElementById('addTrip').addEventListener('click', (event) => addTrip(event));
