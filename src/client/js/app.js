83ca18aa68be33bea8c36ac4db5ba08f129f4448
Catarina Batista <catarinabatistayou@gmail.com>
2020-01-14 12:18:49 +0000

    Remove functions

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,179 +1,44 @@
-import { getCityData, getImages, getWeather} from "./apis";
 import moment from "moment";
 
-let newTrip = {};
 const all = document.getElementById("allTrips");
-let isScrolling = false;
 const nav = document.getElementById('nav');
+let isScrolling = false;
 
-
-/* --- Show a modal with data of the city --- */
-const showCityData = async(event) => {
-    const form = document.getElementById("form");
-
-    if (form.checkValidity()) {
-        const city = await getCityData(event);
-    
-        fillModalInfo(city);
-        $('#modal').modal({show: true}); 
+/* --- Show or hide navBar after 1s --- */
+const showNavBar = async() => {
+    if (window.pageYOffset === 0) {
+        nav.classList.add("top");
     }
     else {
-        alert("Please fill out the field 'Going to', 'Departing on' and 'Returning on'");
-    }
-}
-
-
-/* --- Fill Modal with Trip Info --- */
-const fillModalInfo = async(city) => {
-    const title = document.getElementById("modalTitle");
-    const startDate = document.getElementById("depart-date").value.toString();
-    const endDate = document.getElementById("return-date").value;
-    const from = document.getElementById("from").value;
-    const carousel = document.querySelectorAll(".img-carousel");
-    const forecast = await getWeather(city.latitude, city.longitude, startDate);
-    let images = await getImages(city.cityName);
-
-
-    if (from === "") {
-        title.innerText = `Going to ${city.cityName}, ${city.country}`;
-    }
-    else {
-        title.innerText = `${from}  -->  ${city.cityName}, ${city.country}`;
-    }
-
-    document.getElementById("departDate").innerText = moment(startDate).format("dddd, DD MMMM YYYY").toString();
-    document.getElementById("returnDate").innerText = moment(endDate).format("dddd, DD MMMM YYYY").toString();
-    document.getElementById("durationTrip").innerText = moment(endDate).diff(moment(startDate), "days") + " days";
-    document.getElementById("forecast").innerText = `${forecast.summary}, ${Math.round(Number(forecast.temperature - 32) * 5 / 9)}ºC  0  ${Math.round(Number(forecast.temperature))}ºF`;
-    document.getElementById("icon").className = await forecastIcon(forecast.icon);
-
-    // Carousel
-    for (let index = 0; index < carousel.length; index++) {
-        const img = carousel[index];
-        const url = images[index];
+        nav.classList.remove("top");
+        if (!isScrolling) {
+            isScrolling = true;
+            nav.classList.remove("hide");
         
-        img.src = url.image;
-        img.alt = url.cityName
-    }
-
-    const generateID = '_' + Math.random().toString(36).substr(2, 9);
-
-    // Trip
-    newTrip = {
-        id: generateID,
-        city: city.cityName,
-        country: city.country,
-        latitude: city.latitude,
-        longitude: city.longitude,
-        startDate: startDate,
-        endDate: endDate,
-        images: images,
-        forecast: forecast
-    }
-}
-
-/* --- Add trip --- */
-const addTrip = async (event) => {
-    event.preventDefault();
-    
-    $("#modal").modal('hide');
-    
-    const response = await fetch("http://localhost:8000/addTrip", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ trip: newTrip })
-    });
-
-    try {
-        const tripData = await response.json();
-        
-        await showTrip(newTrip);
-
-        return tripData;
-
-    } catch (error) {
-        console.log(error);
-        alert(error)
-    }
-
-}
-
-/* --- Show the new trip in all trips section --- */
-const showTrip = async (trip) => {
-    document.getElementById("section-trips").classList.remove("hidden");
-    document.getElementById("li-trips").style.display = "block";
-    
-    await addCard(trip);
-}
-
-/* --- Show all trips when reload the page in all trips section --- */
-const showAllTrips = async () => {
-    nav.classList.add("top");
-
-    const trips = await fetch("http://localhost:8000/getTrips")
-        .then(Response => Response.json());
-
-    try { 
-        if (trips.length > 0) {
-            for (const trip of trips) {
-                await addCard(trip);
-            }
-
-            document.getElementById("section-trips").classList.remove("hidden");
-            document.getElementById("li-trips").style.display = "block";
+            setTimeout(function() {
+                isScrolling = false;
+                nav.classList.add("hide");
+            }, 1000);
         }
-    }
-    catch (error) {
-        console.log("ERROR: ", error);
-        alert(error);
-    }
+    } 
+};
+
+
+/* --- Option to print trip --- */
+const printTrip = async (event) => {
+    window.print();
 }
 
-/* Add/Fill the cards of trips */
-const addCard = async (trip) => {
-    const card = document.createElement("section");
 
-    card.innerHTML = 
-        `<card class="card">
-            <div class="carousel slide" data-ride="carousel">
-                <div id="cardImg" class="carousel-inner" id="carousel2">
-                    <div class="carousel-item active">
-                        <img class="img-card" src="${trip.images[0].image}" alt="">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img-card" src="${trip.images[1].image}" alt="">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img-card" src="${trip.images[2].image}" alt="">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img-card" src="${trip.images[3].image}" alt="">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img-card" src="${trip.images[4].image}" alt="">
-                    </div>
-                </div>
-            </div>
+/* --- Disallow select dates before today --- */
+const restrictDates = async() => {
+    let today = new Date();
+    today =  today.getFullYear() + "-" + String(today.getMonth() + 1).padStart(2, '0') + "-" + String(today.getDate()).padStart(2, '0');
 
-            <div class="card-body">
-                <h5 class="card-title">${trip.city}, ${trip.country}</h5>
-                <div class="card-text">
-                    <i class="fas fa-plane"></i><b>Departing on</b>:<br>
-                    <p>${moment(trip.startDate).format("ddd, DD MMMM YYYY").toString()}</p> <br>
-                    <i class="fas fa-suitcase-rolling"></i><b>Returning on</b>: <br>
-                    <p>${moment(trip.endDate).format("ddd, DD MMMM YYYY").toString()}</p> <br>
-                    <i class="fas fa-hourglass-half"></i><b>Duration</b>: <br> 
-                    <p>${moment(trip.endDate).diff(moment(trip.startDate), "days")} days </p><br>
-                    <i class="${await forecastIcon(trip.forecast.icon)}"></i><b>Forecast</b>: <br>
-                    <p>${Math.round(Number(trip.forecast.temperature - 32) * 5 / 9)}ºC, ${trip.forecast.summary}</p> <br>
-                </div>
-                <button id="delete" class="btn btn-danger">Delete</button>
-            </div>
-        </card>`;
-        
-    all.appendChild(card);
-        
+    document.getElementById("return-date").min = today.toString();
+    document.getElementById("depart-date").min = today.toString();
 }
+
 
 /* --- Select the icon according to forecast --- */
 const forecastIcon = async(icon) => {
@@ -214,37 +79,50 @@ const forecastIcon = async(icon) => {
 }
 
 
-/* --- Show or hide navBar it after 1s --- */
-const showNavBar = async() => {
-    if (window.pageYOffset === 0) {
-        nav.classList.add("top");
-    }
-    else {
-        nav.classList.remove("top");
-        if (!isScrolling) {
-            isScrolling = true;
-            nav.classList.remove("hide");
+/* Add/fill the cards of trips */
+const addCard = async (trip) => {
+    const card = document.createElement("section");
+
+    card.innerHTML = 
+        `<card class="card">
+            <div class="carousel slide" data-ride="carousel">
+                <div id="cardImg" class="carousel-inner" id="carousel2">
+                    <div class="carousel-item active">
+                        <img class="img-card" src="${trip.images[0].image}" alt="">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="img-card" src="${trip.images[1].image}" alt="">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="img-card" src="${trip.images[2].image}" alt="">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="img-card" src="${trip.images[3].image}" alt="">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="img-card" src="${trip.images[4].image}" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-body">
+                <h5 class="card-title">${trip.city}, ${trip.country}</h5>
+                <div class="card-text">
+                    <i class="fas fa-plane"></i><b>Departing on</b>:<br>
+                    <p>${moment(trip.startDate).format("ddd, DD MMMM YYYY").toString()}</p> <br>
+                    <i class="fas fa-suitcase-rolling"></i><b>Returning on</b>: <br>
+                    <p>${moment(trip.endDate).format("ddd, DD MMMM YYYY").toString()}</p> <br>
+                    <i class="fas fa-hourglass-half"></i><b>Duration</b>: <br> 
+                    <p>${moment(trip.endDate).diff(moment(trip.startDate), "days")} days </p><br>
+                    <i class="${await forecastIcon(trip.forecast.icon)}"></i><b>Forecast</b>: <br>
+                    <p>${Math.round(Number(trip.forecast.temperature - 32) * 5 / 9)}ºC, ${trip.forecast.summary}</p> <br>
+                </div>
+                <button class="btn btn-danger delete-btn" data-id="${trip.id}">Delete</button>
+            </div>
+        </card>`;
         
-            setTimeout(function() {
-                isScrolling = false;
-                nav.classList.add("hide");
-            }, 1000);
-        }
-    } 
-};
-
-/* --- Option to print --- */
-const printTrip = async (event) => {
-    window.print();
+    all.appendChild(card);        
 }
 
-/* --- Disallow select dates before today --- */
-const restrictDates = async() => {
-    let today = new Date();
-    today =  today.getFullYear() + "-" + String(today.getMonth() + 1).padStart(2, '0') + "-" + String(today.getDate()).padStart(2, '0');
 
-    document.getElementById("return-date").min = today.toString();
-    document.getElementById("depart-date").min = today.toString();
-}
-
-export { showNavBar, showAllTrips, showCityData, addTrip, printTrip, restrictDates };
+export { showNavBar, printTrip, restrictDates, forecastIcon, addCard };
